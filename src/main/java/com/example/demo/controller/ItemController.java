2f30fa4e4d7339ab213c8c018ac0e49f0690d553
Khade Renuka R <136619825+renuka2101476@users.noreply.github.com>
2026-02-10 15:14:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Item;
+import com.example.demo.service.ItemService;
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController
+{
+
+	private ItemService itemService;
+	
+	public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> addItem(@Valid @RequestBody Item item) {
+        return ResponseEntity.ok(itemService.addItem(item));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getItem(@PathVariable Long id) {
+        return itemService.getItemById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
+
